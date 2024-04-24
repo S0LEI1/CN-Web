9d8f0ce068b105a755d79e1aa27637261ad17cfd
S0LEI1 <vanngocpentakill@gmail.com>
2024-04-24 16:15:23 +0700

    .

---
+++ js/phongkhach.js
@@ -0,0 +1,74 @@
+let product1 = {
+  name: "Ghế sofa phòng khách bằng gỗ cao su tự nhiên giá rẻ MOHO MILAN 902",
+  image:
+    "../img/phongkhach/sofa/ghe-sofa-doi-phong-khach-bang-go-tram-tu-nhien-moho-vline-601-homeazvn-1221010968.png",
+  price: "5.990.000",
+  producer: "Nội thất MOHO",
+  type: "sofa",
+};
+let product2 = {
+  name: "Bộ ghế sofa góc chữ L gỗ cao su tự nhiên kèm đôn MOHO VLINE 601",
+  image:
+    "../img/phongkhach/sofa/ghe-sofa-doi-phong-khach-bang-go-tram-tu-nhien-moho-vline-601-homeazvn-1221010968.png",
+  price: "15.970.000",
+  producer: "Nội thất MOHO",
+  color: ["Nâu", "Màu gỗ"],
+  type: "sofa",
+};
+let product3 = {
+  name: "Ghế sofa đôi bằng gỗ cao su tự nhiên MOHO MILAN 901",
+  image:
+    "../img/phongkhach/sofa/ghe-sofa-doi-bang-go-cao-su-tu-nhien-moho-milan-901-homeazvn-1211447236.png",
+  price: "5.490.000đ",
+  producer: "Nội thất MOHO",
+  type: "sofa",
+};
+let product4 = {
+  name: "Ghế sofa góc chữ I gỗ cao su tự nhiên MOHO VLINE 601",
+  image:
+    "../img/phongkhach/sofa/ghe-sofa-phong-khach-cao-cap-go-tram-tu-nhien-moho-kolding-701-homeazvn-1224554720.png",
+  price: "13.990.000 đ",
+  producer: "Nội thất MOHO",
+  type: "sofa",
+};
+let product5 = {
+  name: "Ghế đôn vuông bằng gỗ cao su tự nhiên hiện đại giá rẻ VLINE 601",
+  image:
+    "../img/phongkhach/banghe/ghe-don-vuong-bang-go-cao-su-tu-nhien-hien-dai-gia-re-vline-601-homeazvn-3085210742.png",
+  price: "1.990.000đ",
+  color: ["Nâu", "Màu gỗ"],
+  producer: "Nội thất MOHO",
+  type: "table-chair",
+};
+let product6 = {
+  name: "Combo bọc ghế lười hạt xốp màu đỏ hoa văn cỡ 70x70cm",
+  image:
+    "../img/phongkhach/banghe/combo-boc-ghe-luoi-hat-xop-mau-do-hoa-van-co-70x70cm-homeazvn152456315.jpg",
+  price: "835.000 đ",
+  producer: "Nội thất MOHO",
+  type: "table-chair",
+};
+let product7 = {
+  name: "Combo vỏ bọc ghế lười + hạt xốp màu ghi sáng size 70x70x70cm",
+  image:
+    "../img/phongkhach/banghe/combo-vo-boc-ghe-luoi-hat-xop-mau-ghi-sang-size-70x70x70cm-homeazvn162431990.png",
+  price: "1.990.000đ",
+  producer: "Nội thất MOHO",
+  type: "table-chair",
+};
+let product8 = {
+  name: "Ghế đôn vuông bằng gỗ cao su tự nhiên hiện đại giá rẻ VLINE 601",
+  image:
+    "../img/phongkhach/banghe/ghe-don-vuong-bang-go-cao-su-tu-nhien-hien-dai-gia-re-vline-601-homeazvn-3085210742.png",
+  price: "1.990.000đ",
+  producer: "Nội thất MOHO",
+  type: "table-chair",
+};
+let product9 = {
+  name: "Combo vỏ ghế lười nâu vàng + hạt xốp kích cỡ 70x70cm",
+  image:
+    "../img/phongkhach/banghe/ghe-don-vuong-bang-go-cao-su-tu-nhien-hien-dai-gia-re-vline-601-homeazvn-3085210742.png",
+  price: "1.990.000đ",
+  producer: "Nội thất MOHO",
+  type: "table-chair",
+};
